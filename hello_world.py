e8fbfc42bafd5b3e968293e4252644db0e161802
Andreas Hopfgartner <andreas.hopfgartner@microsoft.com>
2025-02-24 14:05:41 +0100

    Add mlflow

--- hello_world.py
+++ hello_world.py
@@ -6,10 +6,10 @@ ray.init()
 def f():
   import emoji
   import mlflow
-  #mlflow.set_tracking_uri("databricks")
-  #mlflow.set_experiment("/Users/andreas.hopfgartner@microsoft.com/hello-world-experiment")
-  #with mlflow.start_run():
-  #  mlflow.log_metric("hello_world_metric", 1)
+  mlflow.set_tracking_uri("databricks")
+  mlflow.set_experiment("/Users/andreas.hopfgartner@microsoft.com/hello-world-experiment")
+  with mlflow.start_run():
+   mlflow.log_metric("hello_world_metric", 1)
   return emoji.emojize('Python is :thumbs_up:')
 
 print(ray.get(f.remote()))
